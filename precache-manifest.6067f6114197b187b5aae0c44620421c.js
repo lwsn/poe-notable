4c8408aa79dd1e38c572b082149ea360fed5fcc7
Lowe Stålnacke <lowe.stalnacke@konsult.atg.se>
2020-03-19 12:50:59 +0100

    Updates

---
+++ precache-manifest.6067f6114197b187b5aae0c44620421c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54785907fe451b1baf8ea537e2ee1f8e",
+    "url": "/poe-notable/index.html"
+  },
+  {
+    "revision": "55f6adb78501b5e98038",
+    "url": "/poe-notable/static/css/main.88c025de.chunk.css"
+  },
+  {
+    "revision": "5ea47f1c6473eec825a9",
+    "url": "/poe-notable/static/js/2.0888ec34.chunk.js"
+  },
+  {
+    "revision": "0a30a5676556bff3efcfb11745133e81",
+    "url": "/poe-notable/static/js/2.0888ec34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f6adb78501b5e98038",
+    "url": "/poe-notable/static/js/main.e54e82ea.chunk.js"
+  },
+  {
+    "revision": "2db9f60f345d92ee2a03",
+    "url": "/poe-notable/static/js/runtime-main.f827873d.js"
+  }
+]);
